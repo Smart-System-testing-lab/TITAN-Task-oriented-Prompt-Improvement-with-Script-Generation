5e5dce5b2fe95f33683490051c45fb9785d8df71
wangjohn5507 <61547950+wangjohn5507@users.noreply.github.com>
2024-03-23 23:18:14 -0400

    Delete pot directory

--- pot/code_gsmhard.py
+++
@@ -1,4 +0,0 @@
-
-Question: A bookshelf has 12 shelves, and 5 books are placed on each shelf. If each book takes 4 inches of space, how many inches of space are used by all the books on the shelves?
-
-print(ans)
