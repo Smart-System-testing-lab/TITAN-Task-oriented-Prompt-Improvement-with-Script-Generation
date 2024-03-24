545716356cd605f9752520e65f0733396cd769ce
wangjohn5507 <61547950+wangjohn5507@users.noreply.github.com>
2024-03-23 23:18:24 -0400

    Delete counting directory

--- counting/1.py
+++
@@ -1,6 +0,0 @@
-word = "bMPePJnPk"
-char_count = len(word)
-letters = list(word)
-
-print("Number of characters:", char_count)
-print("Letters in array:", letters)
--- counting/extraction.py
+++
@@ -1,4 +0,0 @@
-import regex as re
-def extract_numbers(s):
-    return re.findall(r'\d+', s)[0]
-
