b0ed7c566c9feef74a1bacca97afef8c00b4ab4b
wangjohn5507 <61547950+wangjohn5507@users.noreply.github.com>
2024-03-23 23:27:43 -0400

    Delete messages.py

--- messages.py
+++
@@ -1,3 +0,0 @@
-cot_message = "You are a supportive assistant capable of analyzing each request thoroughly."
-step_back_message = "You are a capable assistant skilled in abstractly interpreting and analyzing requests."
-code_message = "You are a helpful assistant that can write Python code that solves mathematical reasoning questions similarly to the examples that you will be provided."
