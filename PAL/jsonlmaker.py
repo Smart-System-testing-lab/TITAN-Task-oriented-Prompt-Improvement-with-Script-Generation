d2091fc59856384e785ff38c909881013474aeb9
wangjohn5507 <61547950+wangjohn5507@users.noreply.github.com>
2024-03-23 23:17:38 -0400

    Delete PAL directory

--- PAL/jsonlmaker.py
+++
@@ -1,9 +0,0 @@
-files = []
-import json
-with open("results/0/gpt4gsmhardv240vfin1.jsonl") as f:
-        for i, jsonObj in enumerate(f):
-            question_json = json.loads(jsonObj)
-            question_json["i"] = i
-            with open("hard.jsonl", 'a+') as file:
-                # Write the dictionary as a JSON string followed by a newline character
-                file.write(json.dumps(question_json) + '\n')
